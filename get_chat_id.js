089029281a51ab8a3e03d23a407748c2e10aa112
ofirm7 <93068794+ofirm7@users.noreply.github.com>
2025-02-05 21:33:40 +0200

    Version 0.0.2
    * Feature permenent authentication.
    
    Persisntent authentiaction using cache files and auth headers.
    
    * Feature - delete messages
    
    * Remove redundant files.
    
    * Add .wwebjs_auth\session\Crashpad to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Code Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\databases\ to .gitignore.
    
    * Fix loop bug.
    
    * Add few supportive function.
    
    * Better logs.
    
    * Feature/configurable monitored chats (#6)
    
    * Add the feature.
    
    * Improve logs.
    
    * Add .wwebjs_auth/ to .gitignore.
    
    * Remove auth files.
    
    * Feature/improve get chat
    
    * Version 0.0.1 (#7)
    
    * Feature permenent authentication.
    
    Persisntent authentiaction using cache files and auth headers.
    
    * Feature - delete messages
    
    * Remove redundant files.
    
    * Add .wwebjs_auth\session\Crashpad to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Code Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\databases\ to .gitignore.
    
    * Fix loop bug.
    
    * Add few supportive function.
    
    * Better logs.
    
    * Feature/configurable monitored chats (#6)
    
    * Add the feature.
    
    * Improve logs.
    
    * Add .wwebjs_auth/ to .gitignore.
    
    * Remove auth files.
    
    * Add the feature.

--- get_chat_id.js
+++ get_chat_id.js
@@ -1,31 +1,37 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const prompt = require('prompt-sync')();
 
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: { headless: true }
 });
 
-// Generate QR Code for login
 client.on('qr', qr => {
     console.log('Scan this QR Code to login:');
     qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', async () => {
-    console.log('Bot is ready! Searching for chat...');
-
-    const chatNameToFind = 'בדיקה 2'; // Change this to the contact or group name
-    const chats = await client.getChats();
-
-    const foundChat = chats.find(chat => chat.name.toLowerCase() === chatNameToFind.toLowerCase());
-
-    if (foundChat) {
-        console.log(`Found chat: ${foundChat.name}`);
-        console.log(`Chat ID: ${foundChat.id._serialized}`);
-    } else {
-        console.log('Chat not found.');
-    }
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    readline.question('Enter chat name: ', async chatNameToFind => {
+        const chats = await client.getChats();
+        console.log('Received chats!')
+        
+        const foundChat = chats.find(chat => chat.name.toLowerCase() === chatNameToFind.toLowerCase());
+        
+        if (foundChat) {
+            console.log(`Found chat: ${foundChat.name}`);
+            console.log(`Chat ID: ${foundChat.id._serialized}`);
+        } else {
+            console.log('Chat not found.');
+        }
+        readline.close();
+    });
 });
 
 client.initialize()
