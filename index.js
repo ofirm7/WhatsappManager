791dbc64df872f474882b71dc4cb8a6560e4b20f
ofirm7 <93068794+ofirm7@users.noreply.github.com>
2025-02-11 20:06:44 +0200

    Dev (#13)
    * Fix (#12)
    
    * Version 0.0.1 (#7)
    
    * Feature permenent authentication.
    
    Persisntent authentiaction using cache files and auth headers.
    
    * Feature - delete messages
    
    * Remove redundant files.
    
    * Add .wwebjs_auth\session\Crashpad to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Code Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\databases\ to .gitignore.
    
    * Fix loop bug.
    
    * Add few supportive function.
    
    * Better logs.
    
    * Feature/configurable monitored chats (#6)
    
    * Add the feature.
    
    * Improve logs.
    
    * Add .wwebjs_auth/ to .gitignore.
    
    * Remove auth files.
    
    * Version 0.0.2
    
    * Feature permenent authentication.
    
    Persisntent authentiaction using cache files and auth headers.
    
    * Feature - delete messages
    
    * Remove redundant files.
    
    * Add .wwebjs_auth\session\Crashpad to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Code Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\databases\ to .gitignore.
    
    * Fix loop bug.
    
    * Add few supportive function.
    
    * Better logs.
    
    * Feature/configurable monitored chats (#6)
    
    * Add the feature.
    
    * Improve logs.
    
    * Add .wwebjs_auth/ to .gitignore.
    
    * Remove auth files.
    
    * Feature/improve get chat
    
    * Version 0.0.1 (#7)
    
    * Feature permenent authentication.
    
    Persisntent authentiaction using cache files and auth headers.
    
    * Feature - delete messages
    
    * Remove redundant files.
    
    * Add .wwebjs_auth\session\Crashpad to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Code Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\databases\ to .gitignore.
    
    * Fix loop bug.
    
    * Add few supportive function.
    
    * Better logs.
    
    * Feature/configurable monitored chats (#6)
    
    * Add the feature.
    
    * Improve logs.
    
    * Add .wwebjs_auth/ to .gitignore.
    
    * Remove auth files.
    
    * Add the feature.
    
    * Feature/message template (#10)
    
    * Feature permenent authentication.
    
    Persisntent authentiaction using cache files and auth headers.
    
    * Feature - delete messages
    
    * Remove redundant files.
    
    * Add .wwebjs_auth\session\Crashpad to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Code Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\databases\ to .gitignore.
    
    * Fix loop bug.
    
    * Add few supportive function.
    
    * Better logs.
    
    * Feature/configurable monitored chats (#6)
    
    * Add the feature.
    
    * Improve logs.
    
    * Add .wwebjs_auth/ to .gitignore.
    
    * Remove auth files.
    
    * Feature/improve get chat
    
    * Version 0.0.1 (#7)
    
    * Feature permenent authentication.
    
    Persisntent authentiaction using cache files and auth headers.
    
    * Feature - delete messages
    
    * Remove redundant files.
    
    * Add .wwebjs_auth\session\Crashpad to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\Code Cache\ to .gitignore.
    
    * Add .wwebjs_auth\session\Default\databases\ to .gitignore.
    
    * Fix loop bug.
    
    * Add few supportive function.
    
    * Better logs.
    
    * Feature/configurable monitored chats (#6)
    
    * Add the feature.
    
    * Improve logs.
    
    * Add .wwebjs_auth/ to .gitignore.
    
    * Remove auth files.
    
    * Add the feature.
    
    * Add partially.
    
    * Fix client.
    
    * Firebase features.
    
    * Add configurable end of messages in FireBase.

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
-
-let monitoredChats = [];
-fs.readFile('monitored_chats.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    console.log('Check if the file "monitored_chats.txt" exists.');
-    return;
-  }
-  monitoredChats = data.split('\n').map(line => line.trim());
-  console.log('Monitored chats:', monitoredChats);
-});
-
-let messageEnding = null;
-fs.readFile('message_ending.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    console.log('Check if the file "message_ending.txt" exists.');
-    return;
-  }
-  messageEnding = '\n\n' + data.split('\n').map(line => line.trim()).join('');
-});
+const { getMessageEnding, getMainChat, getChats, initializeFirebaseApp } = require('./firebase_tools');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -33,53 +13,122 @@ client.on('qr', qr => {
   qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-  console.log('WhatsApp bot is running and listening for your messages...');
+const getChatId = async (chatNameToFind) => {
+    const chats = await client.getChats();
+    const foundChat = chats.find(chat => chat.name.toLowerCase() === chatNameToFind.toLowerCase());
+    if (foundChat) {
+        return foundChat.id._serialized;
+    } else {
+        console.log(`Chat not found: ${chatNameToFind}!`);
+        return null;
+    }
+};
+
+const fetchMessageEnding = async () => {
+    let messageEndingText;
+    let rawMessageEnding = await getMessageEnding();
+    if ( rawMessageEnding && typeof rawMessageEnding === 'object') {
+            let chatEntries = Object.entries(rawMessageEnding);
+            if (chatEntries.length === 1) {
+                messageEndingText = "\n\n" + chatEntries[0][1];
+            }
+        }
+    return messageEndingText
+};
+
+const fetchMainChatId = async () => {
+    let mainChatId;
+    let mainChat = await getMainChat();
+    if (mainChat && typeof mainChat === 'object') {
+            let chatEntries = Object.entries(mainChat);
+            if (chatEntries.length === 1) {
+                let [mainChatName, chatId] = chatEntries[0];
+                mainChatId = chatId.trim() ? chatId : await getChatId(mainChatName);
+            }
+        }
+    return mainChatId
+};
+
+const fetchTargetChatsIds = async () => {
+    let rawChats = await getChats();
+    let updatedtargetChats = [];
+    if (Array.isArray(rawChats)) {
+        for (const obj of rawChats) {
+            let chatEntries = Object.entries(obj); // Extract key-value pairs
+
+            for (const [chatName, chatId] of chatEntries) {
+                let resolvedChatId = chatId.trim() ? chatId : await getChatId(chatName);
+
+                if (resolvedChatId) {
+                    updatedtargetChats.push(resolvedChatId);
+                } else {
+                    console.error(`Failed to resolve ID for chat: ${chatName}`);
+                }
+            }
+        }
+    } else {
+        console.error("Expected an array from getChats(), but got:", rawChats);
+    }
+    return updatedtargetChats;
+};
+
+let mainChatId;
+let targetChats = [];
+let messageEnding;
+const fetchConfiguration = async () => {
+    messageEnding = await fetchMessageEnding();
+    mainChatId = await fetchMainChatId();
+    targetChats = await fetchTargetChatsIds();
+    
+    console.log("Main Chat ID:", mainChatId);
+    console.log("Raw Chats from Firebase:", JSON.stringify(targetChats, null, 2));
+};
+
+client.on('ready', async () => {
+    await fetchConfiguration()
+    console.log('WhatsApp bot is running and listening for your messages...');
 });
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
 
 async function getMessageFromChat(messageBody, chatId) {
-  messageBody = messageBody.replace(messageEnding, '');
-  let chat = await client.getChatById(chatId);
-  let messages = await chat.fetchMessages({ limit: 20 });
-  return messages.find(m => m.body.replace(messageEnding, '') === messageBody && m.fromMe);
-}
+    messageBody = messageBody.replace(messageEnding, '');
+    let chat = await client.getChatById(chatId);
+    let messages = await chat.fetchMessages({ limit: 20 });
+    return messages.find(m => m.body.replace(messageEnding, '') === messageBody && m.fromMe);
+};
 
 async function isMessageInChat(messageBody, chatId) {
-  let foundMessage = await getMessageFromChat(messageBody, chatId);
-  return foundMessage;
-}
+    let foundMessage = await getMessageFromChat(messageBody, chatId);
+    return foundMessage;
+};
 
 client.on('message_create', async message => {
-  await sleep(5000);
-  if (message.fromMe && monitoredChats.includes(message.to)) {
-    for (const chatId of monitoredChats) {
-      let isMessageInCurrentChat = await isMessageInChat(message.body, chatId);
-      if (!isMessageInCurrentChat) {
-        await client.sendMessage(chatId, message.body + messageEnding);
-        console.log(`Message "${message.body}" forwarded from ${message.to} to ${chatId}`);
-      }
+    await sleep(5000);
+    if (message.fromMe && message.to === mainChatId) {
+        for (const chatId of targetChats) {
+            await client.sendMessage(chatId, message.body + messageEnding);
+            console.log(`Message "${message.body}" forwarded from ${message.to} to ${chatId}`);
+        }
     }
-  }
 });
 
 client.on('message_revoke_everyone', async (after, before) => {
-  if (before && before.fromMe && monitoredChats.includes(before.to)) {
-    for (const chatId of monitoredChats) {
-      try {
-        let messageToDelete = await getMessageFromChat(before.body, chatId);
-        if (messageToDelete) {
-          await messageToDelete.delete(true);
-          console.log(`Deleted the message "${before.body}" in ${chatId}`);
+    if (before && before.fromMe && (targetChats.includes(before.to) | before.to === mainChatId)) {
+        for (const chatId of targetChats) {
+            try {
+                let messageToDelete = await getMessageFromChat(before.body, chatId);
+                if (messageToDelete) {
+                    await messageToDelete.delete(true);
+                    console.log(`Deleted the message "${before.body}" in ${chatId}`);
+                }
+            } catch (err) {
+                console.error(`Error deleting the message "${before.body}" in ${chatId}:`, err);
+            }
         }
-      } catch (err) {
-        console.error(`Error deleting the message "${before.body}" in ${chatId}:`, err);
-      }
     }
-  }
 });
 
 client.initialize();
